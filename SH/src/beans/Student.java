1e3fe1d4a4b4922db6ca297c2e7faf77e093d225
KRISHNA <maddelakrishnakumar@gmail.com>
2017-08-22 18:20:45 +0530

    uploaded

---
+++ SH/src/beans/Student.java
@@ -0,0 +1,27 @@
+package beans;
+
+public class Student {
+	private int sid;
+	private String name,email;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
